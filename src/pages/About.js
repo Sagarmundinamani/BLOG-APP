de77c951b6878331d3609f9b7536e2f8d483048f
Sagar mundinamani <mundinamanisagar@gmail.com>
2025-02-09 23:12:12 +0530

    Updated About.js page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,16 @@ const About = () => {
       <h1>About This Blog</h1>
       <p>This blog is a sample application built with React and React Router.</p>
     </div>
+    
+  );
+};
+const Footer = () => {
+  return (
+      <footer className="bg-gray-800 text-white text-center py-4">
+          <p>&copy; 2025 Sagar Mundinamni. All rights reserved.</p>
+      </footer>
   );
 };
 
-export default About;
+export default Footer;
+
